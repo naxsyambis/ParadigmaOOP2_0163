e8f2b193ab4dc2002eeaa40241944b5e3778a885
naxsyambis <naxsyambis@gmail.com>
2024-05-28 10:23:44 +0100

    membuat class seseorang dan bentuk pure virtual dan virtual function

--- ParadigmaOOPpart2.cpp
+++ ParadigmaOOPpart2.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+using namespace std;
+
+class seseorang
+{
+public:
+    virtual void pesan() = 0; //pure virtual function
+
+    // virtual function biasa
+    //virtual void pesan()
+    // {
+    // cout << "pesan dari seseorang" << endl;
+    // }
+};
+
+
 
 int main()
 {
