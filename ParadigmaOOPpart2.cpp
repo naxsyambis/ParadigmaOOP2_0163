c52da338140cebf78d1bc2ee7503d02bc17e2edb
naxsyambis <naxsyambis@gmail.com>
2024-05-28 10:25:40 +0100

    membuat class joko

--- ParadigmaOOPpart2.cpp
+++ ParadigmaOOPpart2.cpp
@@ -13,6 +13,17 @@ public:
     // }
 };
 
+class joko : public seseorang
+{
+public:
+    // deklarasi
+    void pesan()
+    {
+        //implementasi
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
 
 
 int main()
