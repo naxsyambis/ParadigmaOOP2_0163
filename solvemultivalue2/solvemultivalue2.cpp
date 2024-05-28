d0e7113fc4f486634212484b2776203234c4c2fd
naxsyambis <naxsyambis@gmail.com>
2024-05-28 11:51:09 +0100

    solve multivalue dengan menghapus virtual

---
+++ solvemultivalue2/solvemultivalue2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Orang
+{
+public:
+    int umur;
+
+    Orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class Pekerja : public Orang
+{
+public:
+
+    Pekerja(int pUmur) :
+        Orang(pUmur)
+    {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+};
+
+class Pelajar : public Orang
+{
+public:
+
+    Pelajar(int pUmur) :
+        Orang(pUmur)
+    {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+};
+
+class Budi : public Pekerja, public Pelajar
+{
+public:
+
+    Budi(int pUmur) :
+        Pekerja(pUmur),
+        Pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+int main()
+{
+    Budi a(12);
+
+    return 0;
+}
