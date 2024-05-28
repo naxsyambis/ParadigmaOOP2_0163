67c7cf14dee56226d3ec1b96fb898ad264ae71af
naxsyambis <naxsyambis@gmail.com>
2024-05-28 10:42:02 +0100

    menambahkan derivedclass : public base class dan int main

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -14,10 +14,21 @@ public:
     }
 };
 
-
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Function dari derived class";
+    }
+};
+    
 
 int main()
 {
-    
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
 }
 
